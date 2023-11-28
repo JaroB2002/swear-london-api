284c8d4a2edd3fbb3a3494e647613ae58f91f9f6
lindsaySchool <r0883883@student.thomasmore.be>
2023-11-28 16:58:38 +0100

    feat: add user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 // usermodel importeren
 const User = require('../models/User');
-
+//functies
 const signup = async(req, res, next) => {
     console.log(req.body);
     let username = req.body.username;
@@ -18,5 +18,22 @@ const signup = async(req, res, next) => {
         })
     });
 }
+const login = async(req, res, next) =>{
+    const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
+        res.json({
+            "status": "success",
+            "data": {
+                "user": result
+            }
+        })
+    }).catch(err => {
+        res.json({
+            "status": "error",
+            "message": error
+        })
+    });
+}
 
+// exporteren van de functies
 module.exports.signup = signup;
+module.exports.login = login;
