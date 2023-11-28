54ce948de2f16a6ea6aaffbd2817a917cc36475b
lindsaySchool <r0883883@student.thomasmore.be>
2023-11-28 21:20:21 +0100

    feat: add web tokens to login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,47 +3,72 @@ const User = require('../models/User');
 //webtokens
 const jwt = require('jsonwebtoken');
 //functies
-const signup = async(req, res, next) => {
+//signup functie
+const signup = async (req, res, next) => {
     console.log(req.body);
     let username = req.body.username;
     let password = req.body.password;
+  
+    try {
+      const user = new User({ username: username });
+      await user.setPassword(password);
+      await user.save();
+  
+      //token for validation
+      let token = jwt.sign(
+        {
+          uid: user._id,
+          username: user.username
+        },
+        "SecretWord"
+      );
+  
+      return res.json({
+        status: "success",
+        data: {
+          token: token
+        }
+      });
+    } catch (err) {
+      return res.json({
+        status: "error",
+        message: "Er is een fout opgetreden bij het opslaan van de gebruiker."
+      });
+    }
+  };
 
-    const user = new User({username: username});
-    await user.setPassword(password);
-    await user.save().then(result => {
-        //token for validation
-        let token = jwt.sign({
-            uid: result._id,
-            username: result.username            
-        }, "SecretWord")
-
-        res.json({
-            "status": "success",
-            "data": {
-                "token": token
-            }
-        })
-    }).catch(err => {
-        res.json({
-            "status": "error",
-        })
-    });
-}
+//login functie
 const login = async(req, res, next) =>{
-    const user = await User.authenticate()(req.body.username, req.body.password).then(result => {
-        res.json({
-            "status": "success",
-            "data": {
-                "user": result
-            }
-        })
-    }).catch(err => {
+    try{
+        const result = await User.authenticate()(req.body.username, req.body.password);
+        if (!result.user) {
+            return res.json({
+                status: "failed",
+                message: "Login failed, user not found."
+            });
+        }
+
+        let token = jwt.sign(
+            {
+                uid: result.user._id,
+                username: result.user.username
+            },
+            "SecretWord"
+        );
+
+        return res.json({
+                status: "success",
+                data: {
+                    token: token
+                }
+        });
+    }catch(err) {
         res.json({
             "status": "error",
-            "message": error
-        })
-    });
-}
+            "message": "error"
+        });
+    }
+};
 
 // exporteren van de functies
 module.exports.signup = signup;
