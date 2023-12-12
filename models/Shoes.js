503bb358472c02390a724b6d6e985c35bc26c337
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-12 11:36:14 +0100

    fix: update shoe model

--- models/Shoes.js
+++ models/Shoes.js
@@ -3,13 +3,29 @@ const mongoose = require('mongoose');
 //Create a schema
 const Schema = mongoose.Schema;
 const shoesSchema = new Schema({
-    text: {
-        type:String,
+    name: {
+        type: String,
         required: true
     },
-    user: {
-        type:String,
+    size: {
+        type: String,
         required: true
+    },
+    client: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    compound: {
+        outside_01: { type: String, required: true },
+        laces: { type: String, required: true },
+        inside: { type: String, required: true },
+        outside_2: { type: String, required: true },
+        sole_bottom: { type: String, required: true },
+        sole_top: { type: String, required: true }
     }
 })
 const Shoes = mongoose.model('Shoes', shoesSchema);
