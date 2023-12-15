812feeb150690f48c3ede6b34ab9ffb16e268737
lindsaySchool <r0883883@student.thomasmore.be>
2023-12-15 11:44:21 +0100

    feat: update live order list

--- primus/primus.js
+++ primus/primus.js
@@ -11,17 +11,28 @@ const go = (server) => {
 
     //check if data received from client, then log
     spark.on("data", (data) => {
-      console.log(data);
-      try{
+      try {
         let parsedData = JSON.parse(data);
-        console.log(`received data from client`, parsedData);
-        primus.write({
-          action: "updateCount",
-          data: {
-            count: parsedData.data.ordersCount
-          }
-        });
-      }catch (err){
+        console.log(`received data from client`, parsedData.data);
+
+        if (parsedData.action === "updateList") {
+          //update the orders list
+          primus.write({
+            action: "updateList",
+            data: {
+              shoes: parsedData.data.shoes,
+            },
+          });
+        } else if (parsedData.action === "updateCount") {
+          //update the orders counter
+          primus.write({
+            action: "updateCount",
+            data: {
+              count: parsedData.data.ordersCount,
+            },
+          });
+        }
+      } catch (err) {
         console.log("de error is:", err);
       }
     });
